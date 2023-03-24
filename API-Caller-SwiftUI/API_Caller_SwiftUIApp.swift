ee7cf1f4f3f5f735d507339be7dd5a0d9869d71c
Md_Shafiul_Alam <shafiul.alam@bjitgroup.com>
2023-03-24 14:04:02 +0600

    Initial Commit

---
+++ API-Caller-SwiftUI/API_Caller_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  API_Caller_SwiftUIApp.swift
+//  API-Caller-SwiftUI
+//
+//  Created by ShafiulAlam-00058 on 3/24/23.
+//
+
+import SwiftUI
+
+@main
+struct API_Caller_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
